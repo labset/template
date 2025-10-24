661bc082606afdf67069513565dc3f7b1b81448d
viqueen <hasnae.rehioui@safetyculture.io>
2025-10-24 11:29:08 +1100

    wire up client in frontend

--- platform/frontend/src/context-providers/connect-api/connect-api-context.ts
+++ platform/frontend/src/context-providers/connect-api/connect-api-context.ts
@@ -1,7 +1,10 @@
 import { createContext } from "react";
+import type { Client } from "@connectrpc/connect";
+import type { TodoV1TodoService } from "@labset/template-api-web-sdk";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface ConnectApi {}
+export interface ConnectApi {
+  todoClient: Client<typeof TodoV1TodoService.TodoService>;
+}
 
 export const ConnectApiContext = createContext<ConnectApi | undefined>(
   undefined,
--- platform/frontend/src/context-providers/connect-api/connect-api-provider.tsx
+++ platform/frontend/src/context-providers/connect-api/connect-api-provider.tsx
@@ -1,10 +1,26 @@
 import type { PropsWithChildren } from "react";
 
+import { createConnectTransport } from "@connectrpc/connect-web";
 import { ConnectApiContext } from "./connect-api-context";
+import { createClient } from "@connectrpc/connect";
+import { TodoV1TodoService } from "@labset/template-api-web-sdk";
 
 export const ConnectApiProvider = ({ children }: PropsWithChildren) => {
+  const transport = createConnectTransport({
+    baseUrl: "http://localhost:8080/api",
+    fetch: (input, init) => {
+      return fetch(input, {
+        ...init,
+        credentials: "include",
+        mode: "cors",
+      });
+    },
+  });
+
+  const todoClient = createClient(TodoV1TodoService.TodoService, transport);
+
   return (
-    <ConnectApiContext.Provider value={{}}>
+    <ConnectApiContext.Provider value={{ todoClient }}>
       {children}
     </ConnectApiContext.Provider>
   );
